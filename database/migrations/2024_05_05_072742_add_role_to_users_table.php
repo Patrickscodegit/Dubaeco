2e44cbe527d2cb942705e866a71af3d310b5f8ac
Patrick Lauwers <patrick@belgaco.be>
2024-05-06 10:30:29 +0400

    table_users

--- database/migrations/2024_05_05_072742_add_role_to_users_table.php
+++ database/migrations/2024_05_05_072742_add_role_to_users_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('user'); // Default role is 'user'
+        if (!Schema::hasTable('users')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('role')->default('user'); // Default role is 'user'
             });
+        }
     }
 
     /**

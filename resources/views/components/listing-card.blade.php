0889dced2f3b8e8fd3ace68fe9850f15161ee70e
Patrick Lauwers <patrick@belgaco.be>
2025-08-11 00:20:15 +0400

    feat: make entire listing card clickable

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,18 +1,19 @@
 @props(['listing'])
 
-<x-card>
-  <div class="flex">
-    <img class="w-32 md:w-48 mr-6 object-cover"
-      src="{{$listing->logo ? asset('storage/' . $listing->logo) : asset('/images/no-image.png')}}" alt="" />
-    <div>
-      <h3 class="text-2xl">
-        <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
-      </h3>
-      <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
-      <x-listing-tags :tagsCsv="$listing->tags" />
-      <div class="text-lg mt-4">
-        <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
+<a href="/listings/{{$listing->id}}" class="block hover:shadow-lg transition-shadow duration-200">
+  <x-card>
+    <div class="flex">
+      <img class="w-32 md:w-48 mr-6 object-cover"
+        src="{{$listing->logo ? asset('storage/' . $listing->logo) : asset('/images/no-image.png')}}" 
+        alt="{{$listing->title}}" />
+      <div>
+        <h3 class="text-2xl hover:text-laravel">{{$listing->title}}</h3>
+        <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
+        <x-listing-tags :tagsCsv="$listing->tags" />
+        <div class="text-lg mt-4">
+          <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
+        </div>
       </div>
     </div>
-  </div>
-</x-card>
+  </x-card>
+</a>
